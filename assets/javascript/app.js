dee98c6dcfd723adb3fc736b9f774212857f1980
KimblesAndBits <wearethekimbles@gmail.com>
2019-05-28 10:09:27 -0700

    working chat

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,6 +16,7 @@ var connectedRef = database.ref(".info/connected");
 var connectionsRef = database.ref("/connections");
 var firstPlayer = database.ref("/playerOne");
 var secondPlayer = database.ref("/playerTwo");
+var chatLog = database.ref("/lastChat");
 connectedRef.on("value", function (snap) {
     if (snap.val()) {
         var con = connectionsRef.push("true");
@@ -23,6 +24,7 @@ connectedRef.on("value", function (snap) {
         con.onDisconnect().remove();
         firstPlayer.onDisconnect().remove();
         secondPlayer.onDisconnect().remove();
+        chatLog.onDisconnect().remove();
     }
 });
 
@@ -31,7 +33,7 @@ connectionsRef.on("value", function (snap) {
     if (!playerNum) {
         if (snap.numChildren() === 1) {
             playerNum = 1;
-        } else {
+        } else if (snap.numChildren() === 2) {
             playerNum = 2;
         }
     }
@@ -40,33 +42,44 @@ connectionsRef.on("value", function (snap) {
 
 $(document).ready(function () {
     var p1Name = "", p2Name = "", p1Choice = "", p2Choice = "", winStatus = "";
+    var wins = 0, ties = 0, losses = 0;
     $("#name-submit").on("click", function () {
         event.preventDefault();
-        var player = $("#player-name").val();
-        $("#name-input").empty();
-        if (playerNum === 1) {
-            firstPlayer.set({
-                playerName: player
-            });
-        } else {
-            secondPlayer.set({
-                playerName: player
-            });
+        nameAssign();
+    });
+
+    $('#name-input').keydown(function (event) {
+        if (event.keyCode === 10 || event.keyCode === 13) {
+            event.preventDefault();
+            nameAssign();
+        }
+    });
+
+    $("#chat-submit").on("click", function () {
+        event.preventDefault();
+        chatAssign();
+    });
+
+    $('#chat-input').keydown(function (event) {
+        if (event.keyCode === 10 || event.keyCode === 13) {
+            event.preventDefault();
+            chatAssign();
         }
     });
 
     $(".card-img-top").on("click", function () {
         var choice = $(this).attr("alt");
-        console.log(choice);
-        if (playerNum === 1 && p1Choice === "") {
-            firstPlayer.set({
-                playerOneChoice: choice
-            });
-        } else if (playerNum === 2 && p2Choice === "") {
-            secondPlayer.set({
-                playerTwoChoice: choice
-            });
-        };
+        if (p1Name && p2Name) {
+            if (playerNum === 1 && p1Choice === "") {
+                firstPlayer.set({
+                    playerOneChoice: choice
+                });
+            } else if (playerNum === 2 && p2Choice === "") {
+                secondPlayer.set({
+                    playerTwoChoice: choice
+                });
+            };
+        }
     });
 
     firstPlayer.on("value", function (snap) {
@@ -91,42 +104,111 @@ $(document).ready(function () {
         compareChoices();
     });
 
+    chatLog.on("value", function (snap) {
+        if (p1Name && p2Name) {
+            if (snap.val().chat) {
+                $("#chat-display").prepend(`<p>${snap.val().chat}</p>`)
+            }
+        }
+    });
+
     function showPlayers() {
         if (p1Name && p2Name) {
             $("#player-names").text(`${p1Name} VS ${p2Name}`);
         };
     };
-    
+
     function compareChoices() {
-        if(p1Choice && p2Choice) {
-            if(p1Choice === "paper"){
-                if(p2Choice === "rock"){
+        if (p1Choice && p2Choice) {
+            if (p1Choice === "paper") {
+                if (p2Choice === "rock") {
                     winStatus = `${p1Name} wins with paper!`;
-                }else if (p2Choice === "scissors") {
+                    if(playerNum === 1) {
+                        wins++;
+                    } else if (playerNum === 2) {
+                        losses++;
+                    };
+                } else if (p2Choice === "scissors") {
                     winStatus = `${p2Name} wins with scissors!`;
-                }else{
+                    if(playerNum === 2) {
+                        wins++;
+                    } else if (playerNum === 1) {
+                        losses++;
+                    };
+                } else {
                     winStatus = `You both chose paper! It's a tie!`;
+                    ties++;
                 }
-            }else if(p1Choice === "rock") {
-                if(p2Choice === "scissors"){
+            } else if (p1Choice === "rock") {
+                if (p2Choice === "scissors") {
                     winStatus = `${p1Name} wins with scissors!`;
-                }else if (p2Choice === "paper") {
+                    if(playerNum === 1) {
+                        wins++;
+                    } else if (playerNum === 2) {
+                        losses++;
+                    };
+                } else if (p2Choice === "paper") {
                     winStatus = `${p2Name} wins with paper!`;
-                }else{
+                    if(playerNum === 2) {
+                        wins++;
+                    } else if (playerNum === 1) {
+                        losses++;
+                    };
+                } else {
                     winStatus = `You both chose rock! It's a tie!`;
+                    ties++;
                 }
-            }else{
-                if(p2Choice === "paper"){
+            } else {
+                if (p2Choice === "paper") {
                     winStatus = `${p1Name} wins with scissors!`;
-                }else if (p2Choice === "rock") {
+                    if(playerNum === 1) {
+                        wins++;
+                    } else if (playerNum === 2) {
+                        losses++;
+                    };
+                } else if (p2Choice === "rock") {
                     winStatus = `${p2Name} wins with rock`;
-                }else {
+                    if(playerNum === 2) {
+                        wins++;
+                    } else if (playerNum === 1) {
+                        losses++;
+                    };
+                } else {
                     winStatus = `You both chose scissors! It's a tie!`;
+                    ties++;
                 }
             }
             alert(winStatus);
             p1Choice = "";
             p2Choice = "";
+        };
+    };
+
+    function nameAssign() {
+        var player = $("#player-name").val();
+        $("#name-input").empty();
+        if (playerNum === 1) {
+            firstPlayer.set({
+                playerName: player
+            });
+        } else {
+            secondPlayer.set({
+                playerName: player
+            });
+        }
+    };
+
+    function chatAssign() {
+        var chatText = $("#chat-input").val();
+        $("#chat-input").val("");
+        if (playerNum === 1) {
+            chatLog.set({
+                chat: `${p1Name}: ${chatText}`
+            });
+        } else if (playerNum === 2) {
+            chatLog.set({
+                chat: `${p2Name}: ${chatText}`
+            });
         };
     };
 
